87e0b91670c0f7e7d495eb62c8fa620b657277f9
Ian Li <techotaku@users.noreply.github.com>
2023-11-25 15:40:05 +0800

    feat: support LOGIN as SMTP authentication method.

--- model/option.go
+++ model/option.go
@@ -46,6 +46,7 @@ func InitOptionMap() {
 	common.OptionMap["SMTPPort"] = strconv.Itoa(common.SMTPPort)
 	common.OptionMap["SMTPAccount"] = ""
 	common.OptionMap["SMTPToken"] = ""
+	common.OptionMap["SMTPAuthLoginEnabled"] = strconv.FormatBool(common.SMTPAuthLoginEnabled)
 	common.OptionMap["Notice"] = ""
 	common.OptionMap["About"] = ""
 	common.OptionMap["HomePageContent"] = ""
@@ -155,6 +156,8 @@ func updateOptionMap(key string, value string) (err error) {
 			common.DisplayInCurrencyEnabled = boolValue
 		case "DisplayTokenStatEnabled":
 			common.DisplayTokenStatEnabled = boolValue
+		case "SMTPAuthLoginEnabled":
+			common.SMTPAuthLoginEnabled = boolValue
 		}
 	}
 	switch key {
